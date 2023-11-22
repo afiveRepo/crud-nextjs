0c555d0646cf8fbceae043953ca2536d5396ed9d
apip <apip.site@gmail.com>
2023-11-22 23:02:52 +0700

    add product

---
+++ app/product/page.tsx
@@ -0,0 +1,24 @@
+export default function Product() {
+    return (
+        <div className="p-10 px-10">
+            <table className="table-fixed">
+                <thead>
+                    <tr className="text-center">
+                        <th>Name Product</th>
+                        <th>Price</th>
+                        <th>Total</th>
+                        <th>Tanggal Transaksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="text-center">
+                        <td>sdsadsd</td>
+                        <td>sdasdsad</td>
+                        <td>sdscacas</td>
+                        <td>dsadsa</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
